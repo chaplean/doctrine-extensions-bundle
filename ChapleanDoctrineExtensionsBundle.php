0a3e6f072268bb6351c80009b583169cd296fee0
Valentin Bontemps <valentin@chaplean.com>
2015-08-24 13:56:17 +0200

    clean files

--- ChapleanDoctrineExtensionsBundle.php
+++ ChapleanDoctrineExtensionsBundle.php
@@ -4,6 +4,14 @@ namespace Chaplean\Bundle\DoctrineExtensionsBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+/**
+ * Class ChapleanDoctrineExtensionsBundle.
+ *
+ * @package   Chaplean\Bundle\DoctrineExtensionsBundle
+ * @author    Valentin - Chaplean <valentin@chaplean.com>
+ * @copyright 2014 - 2015 Chaplean (http://www.chaplean.com)
+ * @since     1.0.0
+ */
 class ChapleanDoctrineExtensionsBundle extends Bundle
 {
 }
