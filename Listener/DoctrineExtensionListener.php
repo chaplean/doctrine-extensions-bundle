60b73c8f66bac48000242cae7a4fc032197d9d1b
Tom - Chaplean <tom@chaplean.com>
2016-05-08 15:16:10 +0200

    Upgrade to new bundle structure and adding Gedmo support

---
+++ Listener/DoctrineExtensionListener.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Chaplean\Bundle\DoctrineExtensionsBundle\Listener;
+
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+
+/**
+ * Class DoctrineExtensionListener.
+ *
+ * @package   App\Bundle\RestBundle\Listener
+ * @author    Tom - Chaplean <tom@chaplean.com>
+ * @copyright 2014 - 2016 Chaplean (http://www.chaplean.com)
+ * @since     0.1.0
+ */
+class DoctrineExtensionListener implements ContainerAwareInterface
+{
+    /**
+     * @var ContainerInterface
+     */
+    protected $container;
+
+    /**
+     * @param \Symfony\Component\DependencyInjection\ContainerInterface|null $container
+     *
+     * @return void
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * @param GetResponseEvent $event
+     *
+     * @return void
+     */
+    public function onLateKernelRequest(GetResponseEvent $event)
+    {
+        $translatable = $this->container->get('gedmo.listener.translatable');
+        $translatable->setTranslatableLocale($event->getRequest()->getLocale());
+    }
+
+    /**
+     * @return void
+     */
+    public function onConsoleCommand()
+    {
+        $this->container->get('gedmo.listener.translatable')->setTranslatableLocale($this->container->get('translator')->getLocale());
+    }
+
+    /**
+     * @param \Symfony\Component\HttpKernel\Event\GetResponseEvent $event
+     *
+     * @return void
+     */
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+        $securityContext = $this->container->get('security.context', ContainerInterface::NULL_ON_INVALID_REFERENCE);
+        if (null !== $securityContext && null !== $securityContext->getToken() && $securityContext->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
+            $loggable = $this->container->get('gedmo.listener.loggable');
+            $loggable->setUsername($securityContext->getToken()->getUsername());
+        }
+    }
+}
---
+++ app/AppKernel.php
@@ -0,0 +1,67 @@
+<?php
+
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Config\Loader\LoaderInterface;
+
+/**
+ * Class AppKernel.
+ *
+ * @author    Valentin - Chaplean <valentin@chaplean.com>
+ * @copyright 2014 - 2015 Chaplean (http://www.chaplean.com)
+ * @since     1.0.0
+ */
+class AppKernel extends Kernel
+{
+    /**
+     * @return array
+     */
+    public function registerBundles()
+    {
+        return array(
+            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new Symfony\Bundle\TwigBundle\TwigBundle(),
+            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+            new Symfony\Bundle\MonologBundle\MonologBundle(),
+            new Liip\FunctionalTestBundle\LiipFunctionalTestBundle(),
+            new Chaplean\Bundle\UnitBundle\ChapleanUnitBundle(),
+            new Chaplean\Bundle\DoctrineExtensionsBundle\ChapleanDoctrineExtensionsBundle(),
+        );
+    }
+
+    /**
+     * @return string
+     */
+    public function getRootDir()
+    {
+        return __DIR__;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCacheDir()
+    {
+        return dirname(__DIR__) . '/var/cache/' . $this->getEnvironment();
+    }
+
+    /**
+     * @return string
+     */
+    public function getLogDir()
+    {
+        return dirname(__DIR__) . '/var/logs';
+    }
+
+    /**
+     * @param LoaderInterface $loader Resource loader.
+     *
+     * @return void
+     */
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load(__DIR__ . '/config/config_' . $this->getEnvironment() . '.yml');
+    }
+}
